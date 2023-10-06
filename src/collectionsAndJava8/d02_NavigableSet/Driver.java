94eb9ea80b475364578d74684316eeb3f103cd8a
adem kok <ademk7604@gmail.com>
2023-10-06 12:18:22 +0200

    HashSet

--- src/collectionsAndJava8/d02_NavigableSet/Driver.java
+++ src/collectionsAndJava8/d02_NavigableSet/Driver.java
@@ -7,6 +7,11 @@ import java.util.TreeSet;
 
 public class Driver {
 	public static void main(String[] args) {
+		/*
+		 * SortedSet den kalitim almistir. Generic typ
+		 * ceiling()  verilen nesneye en yakin ve ondan yukuk olan nesneyi bulur
+		 * and headset()
+		 */
 		NavigableSet<Integer> navigableSet = new TreeSet<>();
 		navigableSet.add(2);
 		navigableSet.add(5);
@@ -20,11 +25,12 @@ public class Driver {
 			Integer next =iterator.next();
 			System.out.println(next);
 		}
-		System.out.println(navigableSet.ceiling(0));
-		System.out.println(navigableSet.floor(9));
-		System.out.println(navigableSet.lower(1));
-		System.out.println(navigableSet.higher(4));
-		SortedSet<Integer> sortedSet1 = navigableSet.headSet(5);
+		// buraya kadar sortedSet ile bir farki yok
+		System.out.println(navigableSet.ceiling(0)); //9 olmadigi icin null, // 4 de en yakin buyuk olan 5 gelir.
+		System.out.println(navigableSet.floor(9)); // 9 dan daha kucuk 5 getirir
+		System.out.println(navigableSet.lower(2)); // 2 den daha kucuk 1 var
+		System.out.println(navigableSet.higher(4)); // 4en daha buyuk 5 i getirecektir
+		SortedSet<Integer> sortedSet1 = navigableSet.headSet(5, true); // 5 kadar lan degerleri getirir
 		Iterator<Integer> iterator2 = sortedSet1.iterator();
 		while(iterator2.hasNext()) {
 			System.out.println(iterator2.next());
