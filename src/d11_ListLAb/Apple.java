50a215380b19f369b7f1f628f5021b26f6613e38
adem kok <ademk7604@gmail.com>
2023-10-09 11:05:32 +0200

    BiletSatisi

---
+++ src/d11_ListLAb/Apple.java
@@ -0,0 +1,30 @@
+package d11_ListLAb;
+
+import java.awt.Color;
+
+public class Apple {
+	 int weight;
+	    Color color;
+
+	    public Apple(int weight, Color color) {
+	        this.weight = weight;
+	        this.color = color;
+	    }
+
+	    public int getWeight() {
+	        return weight;
+	    }
+
+	    public Color getColor() {
+	        return color;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Apple{" +
+	                "weight=" + weight +
+	                ", color=" + color +
+	                '}';
+	    }
+
+}
---
+++ src/d11_ListLAb/ListComparator.java
@@ -0,0 +1,32 @@
+package d11_ListLAb;
+
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class ListComparator {
+	public static void main(String[] args) {
+		 List<Apple> list = Arrays.asList(
+	                new Apple(140, Color.RED),
+	                new Apple(190, Color.RED),
+	                new Apple(150, Color.GREEN),
+	                new Apple(100, Color.GREEN),
+	                new Apple(170, Color.RED),
+	                new Apple(110, Color.GREEN)
+	        );
+//	        list.sort(new Comparator<Apple>() {
+//	            @Override
+//	            public int compare(Apple a1, Apple a2) {
+//	                return a1.getWeight() - a2.getWeight();
+//	            }
+//	        });
+//	        System.out.println(list);
+
+	        //list.sort((Apple a1, Apple a2) -> a1.getWeight() - a2.getWeight());
+	        list.sort(Comparator.comparing(Apple::getWeight));
+	        System.out.println(list);
+		
+	}
+
+}
