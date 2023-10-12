0c59e4de5fe30701e8ac706c099a006b0092478b
adem kok <ademk7604@gmail.com>
2023-10-12 15:51:57 +0200

    Threads

--- src/d23_FunctionalInterfaceLab2/Driver2.java
+++ src/d23_FunctionalInterfaceLab2/Driver2.java
@@ -3,21 +3,16 @@ package d23_FunctionalInterfaceLab2;
 import java.util.Date;
 import java.util.Scanner;
 
-public class Driver {
+public class Driver2 {
 	public static void main(String[] args) {
 		System.out.println("Merhaba");
 		Scanner scanner = new Scanner(System.in);
-		Thread kullaniciGirisiThread = new Thread(new Runnable() {
-			// run icinde yapilan islemler ayri bir Thread de calisacaktir uygulamamizi
-			// diger islemleri yapmaya devam edecektir. Yani uyg miz beklemeyecektir.
-			@Override
-			public void run() {
-				while (true) {
-					System.out.print("Giris: ");
-					String line = scanner.nextLine();
-					System.out.println(line.toUpperCase());
-
-				}
+		// Bu sekilde lambda operatoru ile de olusturabiliyoruz Anonym class yerine
+		Thread kullaniciGirisiThread = new Thread(() -> {
+			while (true) {
+				System.out.print("Giris: ");
+				String line = scanner.nextLine();
+				System.out.println(line.toUpperCase());
 			}
 		});
 		kullaniciGirisiThread.start();
